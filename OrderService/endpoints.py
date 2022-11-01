2afa0f14770ff1b95d6e094886f26f8e8e72a4ef
Helgi21 <helgi203@gmail.com>
2022-11-01 21:09:26 +0000

    mostly docker stuff

--- OrderService/endpoints.py
+++ OrderService/endpoints.py
@@ -18,6 +18,6 @@ async def create_order(order: OrderModel,
     return res
 
 @router.get('/orders/{order_id}')
-async def get_order(order_id: int):
-    #Check if order exists
-    pass
+async def get_order(order_id: int,
+                    order_repository: OrderRepository = Depends(Provide[Container.order_repository_provider])):
+    return order_repository.get_order(order_id)
